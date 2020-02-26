edf437554044c62e3ec2dba66e8a839b581b98b5
michael00987 <hyeongkwangseok@gmail.com>
2020-02-26 18:10:01 +0900

    Feat : 로그인 컴포넌트 작성
     - 이미지Tag
     - form Tag 작성 (이메일, 패스워드, submit)

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,24 @@
 import React from 'react';
-// import './App.css';
-// import { Switch, Route } from 'react-router-dom';
+import './Login.css';
 
-class Login extends React.Component {
-  render() {
-    return <div className="App">여기는 로그인입니다.</div>;
-  }
+function Login() {
+  return (
+    <div id="out">
+      <div class="head">로그인</div>
+      <div>
+        <img src="https://source.unsplash.com/1600x900/?code" width="400px" />
+      </div>
+      <div class="이메일 비밀번호 입력 및 제출">
+        <form>
+          이메일
+          <input className="Login_input_email" placeholder="입력" />
+          비밀번호
+          <input className="Login_input_password" placeholder="입력" />
+          <input type="submit" />
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
