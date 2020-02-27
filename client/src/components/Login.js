022b0f898fe79585ab202b2130d90e7233d4b63b
michael00987 <hyeongkwangseok@gmail.com>
2020-02-27 16:24:57 +0900

    Feat : Login컴포넌트에서 회원가입 버튼생성및 화면전환
     - 회원가입버튼을 만들고 회원가입페이지로 전환할수 있게 이벤트를 추가했습니다.
     - 버튼 이미지 관련 css 를 추가했습니다.
     - 입력한 이메일 비밀번호에 따라 리액트가 변화를 감지해야 하기 때문에 state를 가지는 class state로 변경했습니다.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,28 +1,45 @@
 import React from 'react';
 import './Login.css';
+import { withRouter } from 'react-router-dom';
 
-function Login() {
-  return (
-    <div id="out">
-      <div class="head">로그인</div>
-      <div>
-        <img src="https://source.unsplash.com/1600x900/?code" width="400px" />
+class Login extends React.Component {
+  render() {
+    const { history } = this.props;
+    return (
+      <div id="out">
+        <div class="head">로그인</div>
+        <div>
+          <img src="https://source.unsplash.com/1600x900/?code" width="400px" />
+        </div>
+        <div class="이메일 비밀번호 입력 및 제출">
+          <form>
+            <div>
+              이메일
+              <input className="Login_input_email" placeholder="입력" />
+            </div>
+            <div>
+              비밀번호
+              <input className="Login_input_password" placeholder="입력" />
+            </div>
+            <div className="button_Frame">
+              <span
+                className="Signup_button"
+                type="text"
+                onClick={() => history.push('/signup')}
+              >
+                아직 회원가입을 하지 않으셨나요?
+              </span>
+            </div>
+            <div className="button_Frame">
+              <span className="Login_button" type="text/css">
+                로그인
+              </span>
+            </div>
+          </form>
+        </div>
       </div>
-      <div class="이메일 비밀번호 입력 및 제출">
-        <form>
-          <div>
-            이메일
-            <input className="Login_input_email" placeholder="입력" />
-          </div>
-          <div>
-            비밀번호
-            <input className="Login_input_password" placeholder="입력" />
-          </div>
-          <input className="Login_button" type="submit" />
-        </form>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
-export default Login;
+export default withRouter(Login);
