db6422234cd32d35ed4e7c2fe8f60c35599e5ac5
michael00987 <hyeongkwangseok@gmail.com>
2020-02-28 18:33:17 +0900

    Feat : axios(로그인, 회원가입), Fix : 클래스명 수정
     - GetMsg.js에서 getmethod 일부 작성 완료 했습니다.
     - 컴포넌트, css 에서 className를 통일성 있게 수정했습니다.

--- client/src/components/GetMsg.js
+++ client/src/components/GetMsg.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import '../css/GetMsg.css';
+import axios from 'axios';
 
 class GetMsg extends React.Component {
   render() {
     const { history } = this.props;
     return (
-      <div id="out">
-        {/* <div>
-          <input
-            className="GetMsg_to_CheerBtn"
-            value="로그아웃"
-            type="submit"
-          />
-        </div> */}
-        <div className="GetMsg_title">Message Page</div>
-        <div>
+      <div className="getMsg">
+        <div className="navigation">
+          <div>
+            <input className="to_CheerBtn" value="로그아웃" type="submit" />
+          </div>
+          <div className="title">Message Page</div>
+          <div>
+            <input className="to_CheerBtn" value="나의정보" type="submit" />
+          </div>
+        </div>
+        <div className="body">
           <textarea
-            className="GetMsg_contants"
+            className="contants"
             type="text"
             cols="70"
             rows="30"
@@ -26,10 +28,17 @@ class GetMsg extends React.Component {
         </div>
         <div>
           <input
-            className="GetMsg_to_CheerBtn"
+            className="to_CheerBtn"
             type="submit"
             value="응원메세지 작성하기"
-            onClick={() => history.push('/sendmsg')}
+            onClick={() => {
+              axios({
+                url: 'http://15.164.164.204:4000/message/getMessage',
+                method: 'get',
+              }).then(res => {
+                console.log(res.data.data.id);
+              });
+            }}
           />
         </div>
       </div>
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -24,63 +24,66 @@ class Login extends React.Component {
     const { history, handleIsLogin } = this.props;
 
     return (
-      <div id="out">
-        <div class="head">로그인</div>
+      <div className="login">
+        <div className="head">로그인</div>
         <div>
           <img src="https://source.unsplash.com/1600x900/?code" width="400px" />
         </div>
-        <div class="이메일 비밀번호 입력 및 제출">
-          <form>
-            <div>
-              이메일
-              <input
-                className="Login_input_email"
-                placeholder="입력"
-                onChange={this.handleInputValue('email')}
-              />
-            </div>
-            <div>
-              비밀번호
-              <input
-                className="Login_input_password"
-                placeholder="입력"
-                onChange={this.handleInputValue('password')}
-              />
-            </div>
-            <div className="Login_button_Frame">
-              <input
-                className="Login_to_Signupbutton"
-                type="submit"
-                onClick={() => history.push('/signup')}
-                value=" 아직 회원가입을 하지 않으셨나요?"
-              />
-            </div>
-            <div className="button_Frame">
-              <input
-                type="submit"
-                className="Login_button"
-                type="submit"
-                value="로그인"
-                onClick={e => {
-                  e.preventDefault();
-                  return axios({
-                    method: 'post',
-                    url: 'http://15.164.164.204:4000/user/signin',
-                    data: {
-                      email: email,
-                      password: password,
-                    },
-                  }).then(res => {
-                    handleIsLogin();
-                    history.push('/');
-                    console.log(res);
-                    console.log(res.data);
-                  });
-                  // .catch(err => console.log(err));
-                }}
-              />
-            </div>
-          </form>
+        <div className="이메일 비밀번호 입력 및 제출">
+          <div>
+            <div>이메일</div>
+            <input
+              className="input_email"
+              placeholder="입력"
+              onChange={this.handleInputValue('email')}
+            />
+          </div>
+          <div>
+            <div>비밀번호</div>
+            <input
+              className="input_password"
+              placeholder="입력"
+              onChange={this.handleInputValue('password')}
+            />
+          </div>
+          <div className="SignupButton_Frame">
+            <input
+              className="to_Signupbutton"
+              type="submit"
+              onClick={() => history.push('/signup')}
+              value=" 아직 회원가입을 하지 않으셨나요?"
+            />
+          </div>
+          <div className="button_Frame">
+            <input
+              type="submit"
+              className="login_button"
+              type="submit"
+              value="로그인"
+              onClick={e => {
+                e.preventDefault();
+                return axios({
+                  method: 'post',
+                  url: 'http://15.164.164.204:4000/user/signin',
+                  data: {
+                    email: email,
+                    password: password,
+                  },
+                }).then(res => {
+                  handleIsLogin();
+                  history.push('/');
+                  console.log(res);
+                  console.log(res.data);
+                });
+                // .catch(err => console.log(err));
+              }}
+            />
+          </div>
+        </div>
+        <div className="social_login">
+          <span>트위터</span>
+          <span>구글</span>
+          <span>페이스북</span>
         </div>
       </div>
     );
